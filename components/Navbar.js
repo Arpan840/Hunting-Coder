43e1cd42c31f406702a47ece29bc2a215152a997
Arpan840 <123809535+Arpan840@users.noreply.github.com>
2023-06-10 04:56:16 -0500

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,10 +8,10 @@ const Navbar = () => {
         <nav className={styles.navbar}>
         <ul>
           <Link href="/"><li>Home</li></Link>
-          <Link href="/About"><li>About</li></Link>
+          <Link href="/about"><li>About</li></Link>
           <Link href="/blogpost"><li>Blogs</li></Link>
           <Link href="/Contactus"> <li>Contact Us</li></Link>
-          <Link href="/AddBlogs"> <li>Add Your Own Blogs</li></Link>
+          <Link href="/addBlogs"> <li>Add Your Own Blogs</li></Link>
         </ul>
       </nav>
     </div>
